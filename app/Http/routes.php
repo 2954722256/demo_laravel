c6b48fb369e3d107e274c562a921984b091af149
dodo_lihao 李灏 <2954722256@qq.com>
2016-12-21 15:34:18 +0800

    add
    修改gitignore
    添加404页面
    添加一些路由的例子

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,51 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+//-------- helloworld
 Route::get('/', function () {
     return view('welcome');
 });
+
+//-------- funny test
+
+Route::get('/dodo', function () {
+    return view('welcomeDodo');
+});
+
+Route::get('/dodostr', function () {
+    return "string";
+});
+
+//-------- 参数理解
+
+Route::get('/dodo/{id}', function($id){
+    return 'User '.$id;
+});
+
+Route::get('/dodo/view', function () {
+    return view('welcomeDodo');
+});
+
+
+//--------
+
+Route::get('/dodoc0/{id?}', function($id = null){
+    return 'User '.$id;
+});
+
+Route::get('/dodoc/{name?}', function($name = 'dodo')
+{
+    return 'User '.$name;
+});
+
+Route::get('/dodoc2/{name}', function($name)
+{
+    return 'User '.$name;
+})
+-> where('name', '[A-Za-z]+');
+
+Route::get('/dodoc3/{id}/{name}', function($id, $name)
+{
+    return 'id : ' .$id .' , User : '.$name;
+})
+->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>dodo | 404</title>
+    <!--bootstrap框架-->
+        <style>
+.not-fount {
+    height: 500px;
+    left: 0;
+    margin-top: -250px;
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    width: 100%;
+}
+.not-fount h4 {
+    color: #666;
+    font-size: 22px;
+    margin-top: 50px;
+}
+.not-fount div {
+    margin-top: 100px;
+}
+.not-fount .but-a {
+    margin: 0 50px;
+}
+.not-fount .but-a:last-child {
+    background-color: #498e3d;
+}
+.but-a:link, .but-a:visited, .but-a:hover, .but-a:active {
+    background-color: #f08200;
+    border-radius: 5px;
+    color: #fff;
+    display: inline-block;
+    font-size: 20px;
+    line-height: 40px;
+    margin: 0 10px;
+    text-align: center;
+    width: 150px;
+}
+.not-fount .but-a {
+    margin: 0 50px;
+	text-decoration: none;
+}
+.not-fount .but-a:last-child {
+    background-color: #498e3d;
+}
+
+ html, body {
+     height: 100%;
+ }
+
+body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    display: table;
+    font-weight: 100;
+    font-family: 'Lato';
+}
+
+.container {
+    text-align: center;
+    display: table-cell;
+    vertical-align: middle;
+}
+
+.content {
+    text-align: center;
+    display: inline-block;
+}
+
+.title {
+    font-size: 96px;
+}
+	</style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="content">
+            <div class="title">404  <br>  noPage Dodo</div>
+        </div>
+    </div>
+</body>
+</html>
